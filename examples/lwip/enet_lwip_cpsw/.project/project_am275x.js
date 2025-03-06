78688d8b045dacb43ba2c933b468a02287a2ee4c
thilak <t-karanam@ti.com>
2025-03-06 23:14:22 +0530

    am275x: enet: Add enet lwip example for TG721
    - Add enet lwip example for TG721
    - Rename the existing example.
    
    Fixes: SITSW-4015
    
    Signed-off-by: Thilak <t-karanam@ti.com>

--- examples/lwip/enet_lwip_cpsw/.project/project_am275x.js
+++ examples/lwip/enet_lwip_cpsw/.project/project_am275x.js
@@ -18,7 +18,7 @@ const files = {
 const filedirs = {
     common: [
         "..",       /* core_os_combo base */
-        "../../..", /* Example base */
+        "../../../..", /* Example base */
     ],
 };
 
@@ -183,7 +183,8 @@ const templates_freertos_a53 =
 ];
 
 const buildOptionCombos = [
-    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am275x-evm", os: "freertos"},
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am275x-evm-dp83867/am275x-evm", os: "freertos"},
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am275x-evm-dp83tg721/am275x-evm", os: "freertos"},
 ];
 
 function getComponentProperty() {
