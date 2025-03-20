264a907879ee9aad84d4e8ff1ee4a2027875009e
Teja Krishna Sai <t-rowthu@ti.com>
2025-03-20 19:22:17 +0530

    am62x/am62ax/am62dx/am62px/am275x: Fix pinmux linking for RMII
    - Fix pinmux generation and linking for k3 devices to
      be able to turn on or off the signals, per port.
    
    Fixes: SITSW-6741
    
    Signed-off-by: Teja Krishna Sai <t-rowthu@ti.com>

--- sysconfig/networking/.meta/enet_cpsw/am62dx_am62ax_am62px/enet_cpsw_pinmux.syscfg.js
+++ sysconfig/networking/.meta/enet_cpsw/am62dx_am62ax_am62px/enet_cpsw_pinmux.syscfg.js
@@ -164,7 +164,7 @@ function getInterfaceNameList(inst) {
     }
     else if (inst.phyToMacInterfaceMode === "RMII")
     {
-        interfaceNameList.push(getInterfaceName(inst, "RMII2"));
+        interfaceNameList.push(getInterfaceName(inst, "RMII1"));
         interfaceNameList.push(getInterfaceName(inst, "RMII2"));
     }
     else
