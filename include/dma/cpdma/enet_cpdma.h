ce0ea7f98d69f97d27940f32eb48312fa662481e
Surbhi Kapoor <s-kapoor1@ti.com>
2025-06-13 14:40:45 +0530

    allsoc: cpdma: Revert cacheline alignment
    Revert cacheline alignment for CPDMA devices due to limited memory
    
    Signed-off-by: Surbhi Kapoor <s-kapoor1@ti.com>

--- include/dma/cpdma/enet_cpdma.h
+++ include/dma/cpdma/enet_cpdma.h
@@ -86,7 +86,7 @@ extern "C" {
 
 #if ((__ARM_ARCH == 7) && (__ARM_ARCH_PROFILE == 'R'))
 /*! * \brief Set the cacheline alignment size */
-#define ENETDMA_CACHELINE_ALIGNMENT            (64U)
+#define ENETDMA_CACHELINE_ALIGNMENT            (32U)
 #else
 #error "Enet library compilation not supported on non cortex R cores. Update correct cache line size"
 #endif
