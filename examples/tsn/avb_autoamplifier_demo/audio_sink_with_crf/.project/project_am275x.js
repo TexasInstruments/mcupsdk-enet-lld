1ccdecc80e12cdf2eefbebcf25778b54649d170b
thilak <t-karanam@ti.com>
2025-06-27 17:39:46 +0530

    am275x: enet: Add Multistream CRF and TCP Server Examples
    - This enables Multistream CRF + TCP Server examples.
    
    Fixes: SITSW-4016,SITSW-4019
    
    Signed-off-by: Thilak <t-karanam@ti.com>

---
+++ examples/tsn/avb_autoamplifier_demo/audio_sink_with_crf/.project/project_am275x.js
@@ -0,0 +1,251 @@
+let path = require('path');
+
+let device = "am275x";
+
+const files = {
+    common: [
+        "gptp_init.c",
+        "autoamp_avtp_init.c",
+        "default_flow_cfg.c",
+        "tsninit.c",
+        "debug_log.c",
+        "tsnapp_lwip_cpsw_main.c",
+        "app_tcpserver.c",
+        "default_flow_cpsw.c",
+        "main.c",
+        "sample_audio.c",
+        "aaf_pcm_app.c",
+        "est_configure.c",
+        "qosapp_misc.c",
+        "crf_testclient.c",
+        "media_clock_ctrl.c",
+    ],
+};
+
+/* Relative to where the makefile will be generated
+ * Typically at <example_folder>/<BOARD>/<core_os_combo>/<compiler>
+ */
+const filedirs = {
+    common: [
+        "..",                      /* core_os_combo base */
+        "../../../..",                /* Example base */
+        "../../../../../",            /* Example base */
+        "../../../../../..",          /* Example base */
+        "../../../../../../enet_media_clock_ctrl", /* Example base */
+        "../../../../../../nrt_flow", /* Example base */
+    ],
+};
+
+const libdirs_freertos = {
+    common: [
+        "generated",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/board/lib",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/lib",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/lib",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/tsn-stack/eval_lib",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/tsn-stack/license_lib",
+        "${MCU_PLUS_SDK_PATH}/source/networking/lwip/lib",
+    ],
+};
+
+const includes_freertos_r5f = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/board/ethphy/enet/rtos_drivers/include",
+        "${MCU_PLUS_SDK_PATH}/source/board/ethphy/port",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/TI_ARM_CLANG/ARM_CR5F",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am275x/r5f",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/utils/include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/utils/V3",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/include/phy",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/include/core",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/soc/k3/am275x",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/hw_include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/hw_include/mdio/V4",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/examples/tsn",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/tsn-stack",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/tsn-stack/eval_inc",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/tsn-stack/eval_inc/tsn_l2",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/tsn-stack/tsn_unibase",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/tsn-stack/tsn_gptp",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/tsn-stack/tsn_gptp/tilld",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/tsn-stack/tsn_combase/tilld/sitara",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/tsn-stack/tsn_gptp/gptpconf",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/tsn-stack/tsn_uniconf",
+        "${MCU_PLUS_SDK_PATH}/source/networking/tsn/tsn-stack/tsn_uniconf/yangs",
+        "${MCU_PLUS_SDK_PATH}/source/networking/lwip/lwip-stack/src/include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/lwip/lwip-port/include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/lwip/lwip-port/freertos/include",
+        "${MCU_PLUS_SDK_PATH}/source/networking/enet/core/lwipif/inc",
+        "${MCU_PLUS_SDK_PATH}/source/networking/lwip/lwip-stack/contrib",
+        "${MCU_PLUS_SDK_PATH}/source/networking/lwip/lwip-config/am275x",
+    ],
+};
+
+const libs_freertos_r5f = {
+    common: [
+        "freertos.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "drivers.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "enet-cpsw.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "board.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "libc.a",
+        "libsysbm.a",
+        "tsn_combase-freertos.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "tsn_unibase-freertos.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "tsn_gptp-freertos.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "tsn_uniconf-freertos.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "tsn_l2-freertos.am275x.r5f.ti-arm-clang.lib",
+        "yangemb-freertos.am275x.r5f.ti-arm-clang.lib",
+        "lwipif-cpsw-freertos.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "lwip-freertos.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "lwip-contrib-freertos.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const linker_includePath_freertos = {
+    common: [
+        "${PROJECT_BUILD_DIR}/syscfg",
+
+    ],
+};
+
+const defines_r5f = {
+    common: [
+        "SOC_AM275X",
+        "ENET_ENABLE_PER_CPSW=1",
+        'PRINT_FORMAT_NO_WARNING',
+        'SITARA',
+        'NO_GETOPT_LONG=1',
+        'UB_LOGCAT=5',
+        'AVTP_ENABLED=1',
+        'AVTP_HAVE_NO_SIGNAL=1',
+        'GPTP_ENABLED=1',
+        'TSNAPP_LOGLEVEL=\\\"4,ubase:45,cbase:45,uconf:45,gptp:33,lldp:45,avtp:45,nconf:45\\\"',
+        'AVTP_DIRECT_MODE=1',
+        'AUTOAMP_APP_ENABLED=1',
+        'AAF_TX_CLASS_A_APPNO=0', // HWTimer0. Map with stream 00:01:02:03:04:05-00:00
+        'AAF_TX_CLASS_D1_1_APPNO=5', // HWTimer1. Map with stream 00:01:02:03:04:05-00:05
+        'AAF_RX_1_APPNO=4', // Map with stream 00:01:02:03:04:05-00:01
+        'AAF_RX_2_APPNO=2', // Map with stream 00:01:02:03:04:05-00:02
+        'AAF_RX_3_APPNO=3', // Map with stream 00:01:02:03:04:05-00:03
+        'AAF_RX_4_APPNO=1', // Map with stream 00:01:02:03:04:05-00:04
+        'WITH_EST_CONFIG=1',
+        'AVTP_CRF_TALKER_ENABLED=1',
+        'CRF_HAVE_NO_SIGNAL=1'
+    ],
+};
+
+const cflags_r5f = {
+    common: [
+        "--include tsn_buildconf/sitara_buildconf.h",
+    ],
+    release: [
+        "-Oz",
+        "-flto",
+    ],
+};
+
+const lflags_r5f = {
+    common: [
+        "--zero_init=on",
+        "--use_memset=fast",
+        "--use_memcpy=fast"
+    ],
+};
+
+const loptflags_r5f = {
+    release: [
+        "-mcpu=cortex-r5",
+        "-mfloat-abi=hard",
+        "-mfpu=vfpv3-d16",
+        "-mthumb",
+        "-Oz",
+        "-flto"
+    ],
+};
+
+const lnkfiles = {
+    common: [
+        "linker.cmd",
+    ]
+};
+
+const syscfgfile = "../example.syscfg";
+
+const readmeDoxygenPageTag = "EXAMPLES_ENET_CPSW_AVTP";
+
+const templates_freertos_r5f =
+[
+    {
+        input: "source/networking/enet/core/sysconfig/.project/templates/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "EnetApp_mainTask",
+            taskPri : "2",
+            stackSize : "16384",
+        },
+    },
+];
+
+const buildOptionCombos = [
+	{ device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am275x-evm-dp83tg721/am275x-evm", os: "freertos"},
+];
+
+function getComponentProperty() {
+    let property = {};
+
+    property.dirPath = path.resolve(__dirname, "..");
+    property.type = "executable";
+    property.name = "crf_multistream_audio_sink";
+    property.isInternal = false;
+    property.buildOptionCombos = buildOptionCombos;
+
+    return property;
+}
+
+function getComponentBuildProperty(buildOption) {
+    let build_property = {};
+
+    build_property.files = files;
+    build_property.filedirs = filedirs;
+    build_property.lnkfiles = lnkfiles;
+    build_property.syscfgfile = syscfgfile;
+    build_property.projecspecFileAction = "link";
+    build_property.readmeDoxygenPageTag = readmeDoxygenPageTag;
+    if(buildOption.cpu.match(/r5f*/)) {
+        if(buildOption.os.match(/freertos*/) )
+        {
+            const _ = require('lodash');
+            let libdirs_freertos_cpy = _.cloneDeep(libdirs_freertos);
+            /* Logic to remove generated/ from libdirs_freertos, it generates warning for ccs build */
+            if (buildOption.isProjectSpecBuild === true)
+            {
+                var delIndex = libdirs_freertos_cpy.common.indexOf('generated');
+                if (delIndex !== -1) {
+                    libdirs_freertos_cpy.common.splice(delIndex, 1);
+                }
+            }
+            build_property.includes = includes_freertos_r5f;
+            build_property.libdirs = libdirs_freertos_cpy;
+            build_property.libs = libs_freertos_r5f;
+            build_property.templates = templates_freertos_r5f;
+            build_property.defines = defines_r5f;
+            build_property.cflags = cflags_r5f;
+            build_property.lflags = lflags_r5f;
+            build_property.projectspecLnkPath = linker_includePath_freertos;
+            build_property.loptflags = loptflags_r5f;
+        }
+    }
+
+    return build_property;
+}
+
+module.exports = {
+    getComponentProperty,
+    getComponentBuildProperty,
+};
