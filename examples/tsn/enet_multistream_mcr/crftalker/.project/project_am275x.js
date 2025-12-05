d219872c271084b621878a806c13a775c90017d9
thilak <t-karanam@ti.com>
2025-12-05 15:14:38 +0530

    am275x: enet: Add Multistream Media Clock Recovery Docs
    - Add Multistream Media Clock Recovery Docs.
    
    Fixes: SITSW-8087
    
    Signed-off-by: Thilak <t-karanam@ti.com>

--- examples/tsn/enet_multistream_mcr/crftalker/.project/project_am275x.js
+++ examples/tsn/enet_multistream_mcr/crftalker/.project/project_am275x.js
@@ -153,7 +153,7 @@ const lnkfiles = {
 
 const syscfgfile = "../example.syscfg";
 
-const readmeDoxygenPageTag = "EXAMPLES_ENET_CPSW_AVTP";
+const readmeDoxygenPageTag = "EXAMPLES_ENET_AVB_MULTISTREAM_MCR";
 
 const templates_freertos_r5f =
 [
